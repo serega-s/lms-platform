13a47b988e618842810e1aa6629a41ac673687a0
serega-s <comp.it.05@inbox.ru>
2022-02-27 20:07:13 +0300

    added README, requirements

---
+++ README.md
@@ -0,0 +1,12 @@
+# Backend for LMS written with Python and FastAPI
+
+### Now backend in process of developing
+
+<br />
+
+**_How To run server:_**
+
+```
+pip install -r requirements.txt
+python -m lms_platform
+```
--- lms_platform/database.py
+++ lms_platform/database.py
@@ -1,7 +1,3 @@
-import time
-
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
