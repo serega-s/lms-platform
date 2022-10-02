f0801fc7cbf392d38fe96c6f95db5183e5ea8590
serega-s <comp.it.05@inbox.ru>
2022-10-02 19:11:28 +0300

    added new utilite

--- lms_platform/utils.py
+++ lms_platform/utils.py
@@ -1,8 +1,12 @@
+import shutil
 from pathlib import Path
 from typing import Any
 
 
-def static_image_url(path: str, file: Any):
+def build_image_url(path: str, file: Any) -> str:
+    """
+        Build image url from static files
+    """
     static_path = Path(path)
     if not static_path.exists():
         static_path.mkdir(parents=True, exist_ok=True)
@@ -10,3 +14,11 @@ def static_image_url(path: str, file: Any):
     img_url = static_path / file.filename
 
     return str(img_url)
+
+
+def copy_fileobj(image: Any, file: Any):
+    """
+        Write image into Pydantic model
+    """
+    with open(image, 'wb+') as file_obj:
+        shutil.copyfileobj(file, file_obj)
