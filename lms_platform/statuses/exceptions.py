26d89a3e92a26dde1bd3fb78f1d7c4607b0f10d9
serega-s <comp.it.05@inbox.ru>
2022-10-02 19:33:04 +0300

    better statuses

--- lms_platform/statuses/exceptions.py
+++ lms_platform/statuses/exceptions.py
@@ -2,15 +2,21 @@ from fastapi import HTTPException, status
 
 
 def HTTP400Exception():
+    """
+        Raise exception if bad request
+    """
     return HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST, 
+        status_code=status.HTTP_400_BAD_REQUEST,
         detail='Bad Request'
     )
 
 
 def HTTP401Exception():
+    """
+        Raise exception if wrong auth credentials
+    """
     return HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED, 
+        status_code=status.HTTP_401_UNAUTHORIZED,
         detail='Could not validate credentials',
         headers={
             'WWW-Authenticate': "Bearer"
@@ -18,28 +24,40 @@ def HTTP401Exception():
 
 
 def HTTP403Exception():
+    """
+        Raise exception if wrong object owner
+    """
     return HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN, 
+        status_code=status.HTTP_403_FORBIDDEN,
         detail='Wrong object owner'
     )
 
 
 def HTTP404Exception():
+    """
+        Raise exception if object not found
+    """
     return HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, 
+        status_code=status.HTTP_404_NOT_FOUND,
         detail='Object not found'
     )
 
 
 def HTTP409Exception():
+    """
+        Raise exception if object already exists
+    """
     return HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, 
+        status_code=status.HTTP_404_NOT_FOUND,
         detail='Object alredy exists'
     )
 
 
 def HTTP500Exception():
+    """
+        Raise exception if internal server error
+    """
     return HTTPException(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         detail='Something went wrong'
     )
--- lms_platform/statuses/responses.py
+++ lms_platform/statuses/responses.py
@@ -2,12 +2,18 @@ from fastapi import Response, status
 
 
 def HTTP200Response():
+    """
+        Return response if success
+    """
     return Response(
         status_code=status.HTTP_200_OK
     )
 
 
 def HTTP204Response():
+    """
+        Return response if success but no content
+    """
     return Response(
         status_code=status.HTTP_204_NO_CONTENT
     )
